7654bc87c563eed122632406a4a51f0ed320f95b
UjjvalDE <ujjvalvaghasiya01@gmail.com>
2025-03-13 18:09:42 +0100

    chage main page

--- setting/home.js
+++ setting/home.js
@@ -3,10 +3,10 @@ const { body, header, validationResult } = require('express-validator');
 module.exports = {
     BindUrl: function () {
 
-        app.get('/', async (req, res) => {
+        app.get('/landing', async (req, res) => {
             res.render('home', { pageTitle: 'Home', user: null });
         })
-        app.get('/landing', async (req, res) => {
+        app.get('/', async (req, res) => {
             res.render('index', { pageTitle: 'Index', user: null });
         })
 
